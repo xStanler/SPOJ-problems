fd04e896b6d2df1f81262003491036eb368efcaf
Stanisław Chmielewski <kontakt.stanislaw.chmielewski@gmail.com>
2025-06-25 21:35:25 +0200

    Solved with simplest idea using just arrays.

--- Easy/Spoj#1056 Tabelki liczb/tabelki.cpp
+++ Easy/Spoj#1056 Tabelki liczb/tabelki.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
+
+#define MAXN  101
+
+int tabela[MAXN][MAXN];
 
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(0);
 
-	short int t, l, k, i;
-
-	string text;
+	short int t, l, k, i, j;
 
 	cin>>t;
 
 	while(t--)
 	{
 		cin>>l>>k;
-		while(l--)
+		for(i = 0; i < l; i++) {
+			for(j = 0; j < k; j++) {
+				cin>>tabela[i][j];
+			}
+		}
+
+		cout<<'\n';
+
+		for(i = 0; i < l; i++)
 		{
-			cout<<text;
+			if(i == 0) {
+				for(j = 1; j < k; j++) cout<<tabela[i][j]<<' ';
+
+				cout<<tabela[i+1][k-1]<<'\n';
+			}else if(i == l-1) {
+				cout<<tabela[i-1][0];
+
+				for(j = 0; j < k-1; j++) cout<<' '<<tabela[i][j];
+			}else{
+				cout<<tabela[i-1][0]<<' ';
+
+				for(j = 1; j < k-1; j++) cout<<tabela[i][j]<<' ';
+
+				cout<<tabela[i+1][k-1]<<'\n';
+			}
 		}
 	}
 
