991c26a4524358d509205e1d3221e684ba7f56fb
Stanisław Chmielewski <kontakt.stanislaw.chmielewski@gmail.com>
2025-06-27 22:41:21 +0200

    Not solved :(

--- Medium/Spoj#631 Pomiar prędkości/pomiar.cpp
+++ Medium/Spoj#631 Pomiar prędkości/pomiar.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
-/* #include <cmath> */
+#include <bits/stdc++.h>
 
 using namespace std;
+
+#define MAXN 5001
+
+vector<int> minV[MAXN];
+vector<int> pomiary;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0);
 
-	int N, t, pomiar, maxV, minV;
+	int N, t, pomiar, maxV, i;
 
 	cin>>t;
 
@@ -15,19 +19,31 @@ int main() {
 		cin>>N;
 
 		maxV = 0;
-		minV = 0;
 
-		while(N--) {
+		for(i = 0; i < N; i++) {
 			cin>>pomiar;
 
 			maxV += pomiar;
 
-			minV -= pomiar;
-
-			if(minV < 0) minV *= -1;
+			pomiary.push_back(pomiar);		
 		}	
 
-		cout<<minV<<' '<<maxV<<'\n';
+		minV[0].push_back(pomiary[0]);
+		minV[1].push_back(abs(pomiary[0] - pomiary[1]));
+		minV[1].push_back(pomiary[0] + pomiary[1]);
+
+		for(i = 2; i < N; i++)
+		{
+
+			for(auto num: minV[i-1]) {
+				minV[i].push_back(abs(num - pomiary[i]));
+			}
+
+		}
+
+		sort(minV[N-1].begin(), minV[N-1].end());
+
+		cout<<minV[N-1][0]<<' '<<maxV<<'\n';
 	}
 
 	return 0;
